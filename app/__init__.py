3cc0eb84e2b00b9522314f04baa3493be80eae74
bayulalu <bayulalu3@gmail.com>
2023-01-03 06:47:16 +0800

    init

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.sql import func
+import os 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'mysql://root:@127.0.0.1:3306/test-backend2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+from app import routes
